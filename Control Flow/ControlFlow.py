b62766f2b489e49c6977b295733318cbb72e24fc
cleitch001 <02carsyn.leitch@gulllakecs.org>
2020-01-08 09:16:01 -0500

    Add Running Totals

--- Control Flow/ControlFlow.py
+++ Control Flow/ControlFlow.py
@@ -6,3 +6,11 @@ Program: Running total
 This program asks users for five numbers
 It then sums up the numbers
 """
+
+sum = 0
+
+for i in range(5):
+    enter_a_number = input("enter a number: ")
+    sum = sum +enter_a_number
+
+print("\nSum of your number is: " + str(sum))
