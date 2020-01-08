27678a31bb13d04ca00b650ab5fb643ffe1bf846
cleitch001 <02carsyn.leitch@gulllakecs.org>
2020-01-08 09:10:14 -0500

    Initial For Loops Commit

--- Control Flow/ControlFlow.py
+++ Control Flow/ControlFlow.py
@@ -1,41 +1,8 @@
 """
 Programmer: Carsyn Leitch
-Date: 12.20.19
-Program: Guess My Number
+Date: 1.8.20
+Program: Running total
+
+This program asks users for five numbers
+It then sums up the numbers
 """
-
-
-myNumber = 7
-
-print('\nGuess a number between 1 & 10\n')
-
-# Ask Users to Guess
-guess = int(input("Enter a Guess: "))
-
-# Keep asking users to guess my number until
-# it is equal to myNumber
-while guess != myNumber:
-    print("\nNope, guess again: ")
-    guess = int(input("Enter a Guess: "))
-
-print("\nCongratulations, you guessed my number!!!\n")
-
-
-
-
-
-"""
-Programmer: Carsyn Leitch
-Date: 12.20.19
-Program: 1 - 10
-"""
-
-x = 1
-
-# While Loop will see if a condition has been met
-# If not is will run again until the condition
-# Has been met
-
-while x <= 10:
-    print(x)
-    x+=1
