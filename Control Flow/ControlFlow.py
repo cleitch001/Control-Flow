97e9c2c030af8bd14d8ee3c1fb8d399f36de6a8a
cleitch001 <02carsyn.leitch@gulllakecs.org>
2020-01-09 07:59:40 -0500

    Add round & Updated Format 2

--- Control Flow/ControlFlow.py
+++ Control Flow/ControlFlow.py
@@ -5,8 +5,8 @@ Program: Running total
 
 This program asks users for five numbers
 It then sums up the numbers
-
-
+"""
+"""
 sum = 0
 how_many_numbers = int(input("\nHow many numbers would you like to sum up: "))
 print("")
@@ -24,7 +24,6 @@ Date: 1.8.20
 Program:Average Test Scores
 
 This program asks users how many tests they wish to average 
-
 """
 
 total = 0 
