e6e9218f51432de1585e25feb9f9a913a889bb51
cleitch001 <02carsyn.leitch@gulllakecs.org>
2020-01-31 08:54:48 -0500

    Add Greeting

---
+++ Control Flow/ControlFlow.py
@@ -0,0 +1,17 @@
+# Date: 2.3.20
+# Greeting Function
+
+name = input("\nwhat is your name: ")
+
+
+# Create Functions Here
+
+"""
+def greeting(): 
+    print("Hi there " + name + "!")
+    print("Very nice to meet you " + name)
+"""
+
+# Call Functions Here
+
+greeting()
